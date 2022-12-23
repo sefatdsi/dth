bc062a8e617dfe90d221fdd60077ffe0c8ec0d8c
sefatdsi <89995409+sefatdsi@users.noreply.github.com>
2022-12-23 15:29:39 +0600

    Update and rename Star-Jalsha.ts to Star-Jalsha.m3u8

--- Star-Jalsha.ts
+++
@@ -1,8 +0,0 @@
-#EXTM3U 
-#EXT-X-VERSION:3 
-#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000
-http://watchindia.net:8880/live/dharmend/PTukavDWzh/2286.ts?token=1
-http://watchindia.net:8880/live/dharmend/PTukavDWzh/2286.ts?token=2
-http://watchindia.net:8880/live/dharmend/PTukavDWzh/2286.ts?token=3
-http://watchindia.net:8880/live/dharmend/PTukavDWzh/2286.ts?token=4
-
